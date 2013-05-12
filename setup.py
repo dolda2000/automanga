a803365ba47dd030f121be0ca48342c4e79efcf5
Fredrik Tolf <fredrik@dolda2000.com>
2013-05-12 05:43:35 +0200

    Added a distutils script.

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name = "automanga",
+      version = "1",
+      description = "Automanga",
+      packages = ["manga"],
+      scripts = ["automanga"])
