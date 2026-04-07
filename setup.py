c2f976694a607d9e349db5aa7d8cad8d17ee1509
Fredrik Tolf <fredrik@dolda2000.com>
2026-04-07 23:10:05 +0200

    Replace setup.py with a pyproject.toml.

--- setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-from setuptools import setup
-
-setup(name = "automanga",
-      version = "1",
-      description = "Automanga",
-      packages = ["manga"],
-      scripts = ["automanga", "getmanga"])
